639457d802b159469f2e427e1161ffe95ad2c6da
Ann-Annice <annannice98@gmail.com>
2022-10-11 16:47:30 +0530

    viewairlines

---
+++ src/app/viewairlines/viewairlines.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewairlines',
+  templateUrl: './viewairlines.component.html',
+  styleUrls: ['./viewairlines.component.css']
+})
+export class ViewairlinesComponent implements OnInit {
+
+  constructor() { }
+
+airlinesData=[]
+
+  ngOnInit(): void {
+  }
+
+}
